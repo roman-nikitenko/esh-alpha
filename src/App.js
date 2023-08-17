9d75ae75b24e0473f98f7a128b8f1b5d59535c18
Roman Nikitenko <Lomkaaa@ukr.net>
2023-08-17 07:56:45 +0300

    Add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import './App.scss';
-import { Main } from "./components/main/main";
+import logo from './img/logo.svg';
+import welcome from './img/welcome.png'
 
 function App() {
   return (
-  <div className="App">
-    <Main />
-  </div>
+    <>
+      <header className="header">
+        <div className="header__top">
+          <a href="#">
+            <img className="logo" src={logo} alt="logo"/>
+          </a>
+
+          <a href="#" className="icon icon--menu"></a>
+        </div>
+
+        <div className="header__bottom">
+          <img className="header__welcome" src={welcome} alt="Welcome to ESH Aplha"/>
+          <span className="header__text">
+            Lorem ipsum dolor sit amet consectetur. Facilisis auctor a diam cursus amet.
+            Molestie metus morbi purus amet malesuada proin amet facilisis.
+          </span>
+          <button type="submit">Contact us</button>
+        </div>
+      </header>
+
+      <main>
+        <section>
+          <h2>About</h2>
+        </section>
+
+        <section>
+          <h2>Service</h2>
+        </section>
+      </main>
+
+      <footer>Footer</footer>
+    </>
   )
 }
 
