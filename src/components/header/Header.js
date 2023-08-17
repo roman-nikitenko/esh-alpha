cc07688f074fb5145256c7db524f331150033b53
Roman Nikitenko <Lomkaaa@ukr.net>
2023-08-17 06:20:35 +0300

    add starting code

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './header.scss'
+import { Navigation } from "./Navigation";
+
+export const Header = () => {
+  return (
+    <section className="Header">
+      <Navigation />
+    </section>
+  );
+};
